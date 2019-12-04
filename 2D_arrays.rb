10f27d926eea67e3d396c9dbbffb70b631636451
goldbergyossi <yygold@gmail.com>
2019-12-03 20:59:24 -0500

    mini assignment

---
+++ 2D_arrays.rb
@@ -0,0 +1,16 @@
+phonebook = [
+    [6479979770],
+    [6476079636],
+    [6124081775]
+]
+
+phonebook [0] [1] = 7788957558
+phonebook [3] = [3381011540]
+
+for nums in phonebook do
+    for twoD in nums do 
+        print "#{twoD}, "
+    end        
+end 
+
+
